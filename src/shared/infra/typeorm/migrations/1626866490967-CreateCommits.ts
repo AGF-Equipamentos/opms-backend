e102155ab799c96f5ac49248472d7a7f434a87a8
Antonio Costa <juninhomathoni99@gmail.com>
2021-07-21 09:33:21 -0300

    feat: created migration from commits table

---
+++ src/shared/infra/typeorm/migrations/1626866490967-CreateCommits.ts
@@ -0,0 +1,69 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateCommits1626866490967 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'commits',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'part_number',
+            type: 'varchar',
+          },
+          {
+            name: 'qty_delivered',
+            type: 'real',
+          },
+          {
+            name: 'qty',
+            type: 'real',
+          },
+          {
+            name: 'op_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'commits',
+      new TableForeignKey({
+        name: 'OwnerOP',
+        columnNames: ['op_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'ops',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('commits', 'OwnerOP');
+    await queryRunner.dropTable('commits');
+  }
+}
