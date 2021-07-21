50e08b4fc641944a5b18278f57c119c2fa32f0f8
Antonio Costa <juninhomathoni99@gmail.com>
2021-07-21 11:34:40 -0300

    feat: add commit entity
    re #2

---
+++ src/modules/commits/infra/typeorm/entities/Commits.ts
@@ -0,0 +1,40 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import OP from '@modules/ops/infra/typeorm/entities/OP';
+
+@Entity('commits')
+class Commit {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  op_id: string;
+
+  @ManyToOne(() => OP)
+  @JoinColumn({ name: 'op_id' })
+  op: OP;
+
+  @Column()
+  part_number: string;
+
+  @Column()
+  qty: number;
+
+  @Column()
+  qty_delivered: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Commit;
