2ee4840629f61e84362b2720c72967b0e822f5c5
Arthur Pedroti <arthurpedroti@gmail.com>
2022-01-27 14:47:44 -0300

    feat: sentry

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,6 +5,8 @@ import express, { Response, NextFunction, Request } from 'express';
 import cors from 'cors';
 import { errors } from 'celebrate';
 import 'express-async-errors';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
@@ -16,11 +18,32 @@ import '@shared/container';
 
 const app = express();
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(cors());
 app.use(express.json());
+
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 // app.use(rateLimiter);
 app.use(routes);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(errors());
 
@@ -31,8 +54,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
       message: err.message,
     });
   }
-
-  console.error(err);
 
   return response.status(500).json({
     status: 'error',
